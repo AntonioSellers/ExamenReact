0a0a1a23b271c5b2dfdd37c323ae58b149c39a58
Toni <Toni.sf04@gmail.com>
2023-12-04 18:21:54 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+  const DNI_alumno = '48803008K';
+
+  return (
+    <>
+      <h1>Examen de {DNI_alumno}</h1>
+     
+    </>
+  );
+}
+
+export default App;
